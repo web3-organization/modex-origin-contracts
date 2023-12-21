12f632d292a8734928714601ad316b02fa41a179
Mir Imad Ahmed <mirimadahmed@outlook.com>
2023-12-21 13:54:14 +0800

    fix

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 
 
-async function deployContract(contractName, ...args) {
+async function deployContract(contractName: any, ...args: any[]) {
     const Contract = await ethers.getContractFactory(contractName);
     const contract = await Contract.deploy(...args);
     await contract.deployed();
@@ -10,24 +10,54 @@ async function deployContract(contractName, ...args) {
 }
 
 async function main() {
-    // Mock addresses for demonstration
-    // const feeToSetter = '0x0a395e6B3B276fE0eFe4Cf157bD64e7478941A85'; // Replace with actual address
-    // const WETHAddress = '0xeb72756ee12309Eae82a0deb9787e69f5b62949c'; // Replace with actual WETH address
-
-
-    // const quantumCoinPair1 = await deployContract("QuantumCoinRouter", "0x4A708e86bE7b1b53cB0B876dFb97187AB7b9Cd88", WETHAddress);
-
-
+    // deployer and weth address
+    const WETHAddress = '0xeb72756ee12309Eae82a0deb9787e69f5b62949c'; // Replace with actual WETH address
+    const deployer = (await ethers.getSigners())[0];
     // steps: 
     // 1. deploy factory
+    const factory = await deployContract("QuantumCoinFactory", deployer.address);
+    console.log("factory address: ", factory.address);
     // 2. deploy router
+    const router = await deployContract("QuantumCoinRouter", factory.address, WETHAddress);
+    console.log("router address: ", router.address);
     // 3. deploy 10 gem tokens and 1 usdt
+    const gemTokens = ["DiamondToken", "EmberToken", "GarnateToken", "GLDToken", "JadeToken", "OpalToken", "PearlToken", "RubyToken", "SaphireToken", "SilverToken", "USDTToken"];
+    const gemTokenAddresses = [];
+    for (let i = 0; i < gemTokens.length; i++) {
+        const gemToken = await deployContract(gemTokens[i]);
+        console.log(`${gemTokens[i]} address: `, gemToken.address);
+        gemTokenAddresses.push(gemToken.address);
+    }
     // 4. approve 11 tokens to router
+    for (let i = 0; i < gemTokenAddresses.length; i++) {
+        const gemToken = await ethers.getContractAt(gemTokens[i], gemTokenAddresses[i]);
+        await gemToken.approve(router.address, ethers.constants.MaxUint256);
+    }
     // 5. add 10 gem tokens and 1 usdt to factory as pairs
+    for (let i = 0; i < gemTokenAddresses.length - 1; i++) {
+        await factory.createPair(gemTokenAddresses[i], gemTokenAddresses[10]);
+    }
     // 6. add liquidity for each gem token and usdt pair for half of the total supply of each token and 1% of the usdt
+    for (let i = 0; i < gemTokenAddresses.length - 1; i++) {
+        await router.addLiquidity(
+            gemTokenAddresses[i], 
+            gemTokenAddresses[10], 
+            ethers.utils.parseEther("5000000000000000000000000"), 
+            ethers.utils.parseEther("10000000000000000000000000"), 
+            ethers.utils.parseEther("5000000000000000000000000"), 
+            ethers.utils.parseEther("10000000000000000000000000"), 
+            deployer.address, 
+            Date.now() + 1000 * 60 * 10
+            );
+    }
     // 7. deploy pocket index with usdt as base token and router address
+    const pocketIndex = await deployContract("PocketIndex", gemTokenAddresses[10], router.address);
     // 8. add all 10 gem tokens to pocket index
+    for (let i = 0; i < gemTokenAddresses.length - 1; i++) {
+        await pocketIndex.addAsset(gemTokenAddresses[i]);
+    }
     // 9. verify all the contracts
+
 }
 
 main()
